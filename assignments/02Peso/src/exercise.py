a9bf10e4e9c588454a3f032276d635543d7e529a
Jonatandlr <a01735993@itesm.mx>
2021-08-23 17:35:47 -0500

    2

--- assignments/02Peso/src/exercise.py
+++ assignments/02Peso/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+     pesoi=float(input("Dame el peso inicial: "))
+    pesof=float(input("Dame el peso final: "))
+    cmeses=float(input("Dame la cantidad de meses: "))
+    ppmeses=float((pesoi-pesof)/cmeses)
+    print ("Lo que debes bajar por mes es: " + str(ppmeses))
 
 
 
